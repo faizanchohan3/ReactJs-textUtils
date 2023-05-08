7e74824dbb57efef41086d4f63b9bcd6191eb8f2
faizanchohan3 <faizanchohan30@gmail.com>
2023-05-08 14:05:22 +0500

    textutils

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
 
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import Navbar from "./components/navbar";
+import Textform from "./components/Textform";
+import React, { useState } from 'react'
+
 function App() {
+  const [mode,setmode]=useState('light');
+const toggleMode=()=>{
+  
+  if(mode==='dark'){
+setmode('light')
+
+  }
+  else
+  setmode('dark')
+  
+
+}
   return (
     <>
-    <nav ClassName="navbar navbar-expand-lg navbar-light bg-light">
-  <div ClassName="container-fluid">
-    <a ClassName="navbar-brand" href="/">Navbar</a>
-    <button ClassName="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span ClassName="navbar-toggler-icon"></span>
-    </button>
-    <div ClassName="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul ClassName="navbar-nav me-auto mb-2 mb-lg-0">
-        <li ClassName="nav-item">
-          <a ClassName="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li ClassName="nav-item">
-          <a ClassName="nav-link" href="/">Link</a>
-        </li>
-       
-        <li ClassName="nav-item">
-          <a ClassName="nav-link disabled" href="/" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form ClassName="d-flex">
-        <input ClassName="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button ClassName="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-</>
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+    <div className="container">
+    <Textform />
+   
+   </div>
+    </>
   );
 }
 
 export default App;
+
+
---
+++ src/components/Textform.js
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+export default function Textform(props) {
+  const handleemptyclick = () => {
+    changetextarea("");
+  };
+  const handleonchange = (event) => {
+    changetextarea(event.target.value);
+    console.log(event.target.value);
+  };
+  const handleUpperClick = () => {
+    let upp = text.toUpperCase();
+    changetextarea(upp);
+  };
+  const handlelowerClick=()=>{
+
+  let lcase=text.toLowerCase();
+   changetextarea(lcase); 
+  }
+  const handleClicCoPYk=()=>{
+var data=document.getElementById("textbox");
+data.select();
+navigator.clipboard.writeText(data.value);
+
+    }
+      const handleExtraSpace=()=>{
+let newtext=text.split(/[ ]+/);
+changetextarea(newtext.join(" "))
+
+    }
+  const [text, changetextarea] = useState("enter text here");
+  return (
+    <>
+      <div className="mb-3">
+        <h1>{props.header}</h1>
+        <textarea
+          id="textbox"
+          value={text}
+          onChange={handleonchange}
+          cols="150"
+          rows="14"
+          className="tectt"
+        ></textarea>
+      </div>
+      <div className="col-auto">
+        <button
+          type="button"
+          onClick={handleemptyclick}
+          className="btn btn-primary mx-2"
+        >
+          clear
+        </button>
+
+        <button
+          type="button"
+          onClick={handleUpperClick}
+          className="btn btn-danger mx-2"
+        >
+          cnvert to Upper Case
+        </button>
+
+        <button
+          type="button"
+          onClick={handlelowerClick}
+          className="btn btn-dark mx-2" 
+        >
+          cnvert to Lower Case
+        </button>
+        <button
+          type="button"
+          onClick={handleClicCoPYk}
+          className="btn btn-info mx-2"
+        >
+          Click to copy
+        </button>
+        <button
+          type="button"
+          onClick={handleExtraSpace}
+          className="btn btn-success"
+        >
+          Remove extra spacing
+        </button>
+      </div>
+      <div className="containaer">
+     <h3>Total </h3>
+     <p>{text.split(" ").length} words & {text.length} Characters</p>
+
+      </div>
+      <div className="containaer">
+     <h3>Preview</h3>
+     <p>{text}</p>
+
+      </div>
+    
+    </>
+  );
+}
+Textform.defaultProps = {
+  header: "Please Input your Text",
+};
+Textform.Prototype = {
+  header: PropTypes.string.isRequired,
+};
